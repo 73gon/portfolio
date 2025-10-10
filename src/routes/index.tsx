cc86a2d0e7d81495b96d686c4c241898b7619960
73gon <malikmardan03@gmail.com>
2025-10-10 20:55:09 +0200

    refactor: rename PortfolioPage to Portfolio for consistency in routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { PortfolioPage } from '@/pages/portfolio';
+import { Portfolio } from '@/pages/portfolio';
 
 export const Route = createFileRoute('/')({
-  component: PortfolioPage,
+  component: Portfolio,
 });
